0fcb38e292db95293861dacc553360c529cd8815
Joshua Diamond <joshua@radcampaign.com>
2017-05-04 00:17:29 -0400

    fixing provisioner shell

--- setup_assets/ubuntu_wp.sh
+++ setup_assets/ubuntu_wp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DIR=$(pwd)
-echo "Working from $DIR"
-
+BASEDIR="/vagrant/setup_assets"
+echo "SWITCHING directories to $BASEDIR"
+cd $BASEDIR
 #update our box
 apt-get update -y && apt-get upgrade -y
 
@@ -17,15 +18,14 @@ apt-get install -y php5 php5-mysql libapache2-mod-php5 php5-mcrypt php5-common
 #set our webfiles directory to the default var www html directory
 if [ ! -d /vagrant/webfiles ]; then
     mkdir /vagrant/webfiles
-    touch /vagrant/webfiles/README.txt
-    cp $DIR/swdv_README.txt /vagrant/webfiles/swdv_README.txt
+    cp $BASEDIR/swdv_README.txt /vagrant/webfiles/
 fi
 
 rm -rf /var/www/html && ln -s /vagrant/webfiles /var/www/html
 
 #sed install our directory rules if we have an allow block .conf file
-if [ -f ./wp_setup_assets/apache_allow_block.conf ]; then
-	sed -i "/DocumentRoot\ \/var\/www\/html/r $DIR/apache_allow_block.conf" /etc/apach e2/sites-enabled/000-default.conf
+if [ -f $BASEDIR/apache_allow_block.conf ]; then
+	sed -i "/DocumentRoot\ \/var\/www\/html/r $BASEDIR/apache_allow_block.conf" /etc/apache2/sites-enabled/000-default.conf
 fi
 #restart apache
 service restart apache2
@@ -33,3 +33,5 @@ service restart apache2
 #outputs ip address for host file configuration
 echo "Server IP for hosts file "
 ifconfig eth0 | grep inet | awk '{ print $2 }'
+
+cd $DIR
