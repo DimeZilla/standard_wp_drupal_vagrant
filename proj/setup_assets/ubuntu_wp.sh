9de79e4ccf08c6d6cd4f90efb86f18b17947b26a
dimezilla <diamond.joshh@gmail.com>
2018-03-13 16:37:07 -0400

    adding support for php 7.1

--- proj/setup_assets/ubuntu_wp.sh
+++ proj/setup_assets/ubuntu_wp.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+#
 DIR=$(pwd)
 BASEDIR="/vagrant/setup_assets"
 echo "SWITCHING directories to $BASEDIR"
 cd $BASEDIR
 #update our box
 add-apt-repository ppa:ondrej/php
-apt-get update -y && apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
 # This takes a little longer but it ensures that the whole command won't necessarily
 # fail when a package no longer exsists
 dependencies=( "apache2" "libapache2-mod-auth-mysql" "libapache2-mod-php" "mysql-client" )
+
 phpdeps=( "php" "php-mysql" "php-mcrypt" "php-common" "php-xml" "php-gd" )
+php71deps=( "php7.1" "php7.1-mysql" "php7.1-mcrypt" "php7.1-common" "php7.1-xml" "php7.1-gd" )
 
 depInstaller(){
     deps=("${!1}")
@@ -21,8 +24,21 @@ depInstaller(){
     done
 }
 
+install71(){
+    add-apt-repository ppa:ondrej/php
+    apt-get update -y
+    depInstaller php71deps[@]
+}
+
 depInstaller dependencies[@]
-depInstaller phpdeps[@]
+
+if [ $1 == "Y" ]  || [ $1 == "y" ] ; then
+    echo "Installing PHP 7.1"
+    install71
+else
+    echo "Installing latest php"
+    depInstaller phpdeps[@]
+fi
 
 #set our webfiles directory to the default var www html directory
 if [ ! -d /vagrant/webfiles ]; then
@@ -38,13 +54,13 @@ if [ -f $BASEDIR/apache_allow_block.conf ]; then
 fi
 
 # one more update
-apt-get update -y && apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
 #enable mod_rewrite
 a2enmod rewrite
 
 #restart apache
 service apache2 restart
-
 
 cd $DIR
