2949c4f04f8340e2aec8efd04d6a0321708986f7
dimezilla <diamond.joshh@gmail.com>
2018-07-27 12:20:26 -0400

    adding better version control for php in vagrantfile

--- proj/setup_assets/ubuntu_wp.sh
+++ proj/setup_assets/ubuntu_wp.sh
@@ -2,43 +2,50 @@
 #
 DIR=$(pwd)
 BASEDIR="/vagrant/setup_assets"
+VERSION=$1
 echo "SWITCHING directories to $BASEDIR"
 cd $BASEDIR
 #update our box
-add-apt-repository ppa:ondrej/php
 apt-get update -y
 apt-get upgrade -y
 
 # This takes a little longer but it ensures that the whole command won't necessarily
 # fail when a package no longer exsists
-dependencies=( "apache2" "libapache2-mod-auth-mysql" "mysql-client" )
+dependencies=( "apache2" "libapache2-mod-auth-mysql" "mysql-client" "python-software-properties" )
 
 phpdeps=( "php" "php-mysql" "php-mcrypt" "php-common" "php-xml" "php-gd" "libapache2-mod-php" )
-php71deps=( "php7.1" "php7.1-mysql" "php7.1-mcrypt" "php7.1-common" "php7.1-xml" "php7.1-gd" "libapache2-mod-php7.1" )
+phpVersionedDeps=( "php$VERSION" "php$VERSION-mysql" "php$VERSION-mcrypt" "php$VERSION-common" "php$VERSION-xml" "php$VERSION-gd" "libapache2-mod-php$VERSION" )
 
 depInstaller(){
     deps=("${!1}")
     for elem in ${deps[@]}
     do
+      echo "---------------"
+      echo "INSTALLING $elem"
+      echo "---------------"
       apt-get install -y $elem
     done
 }
 
-install71(){
-    add-apt-repository ppa:ondrej/php
+installOther(){
     apt-get update -y
-    depInstaller php71deps[@]
+    depInstaller phpVersionedDeps[@]
 }
 
+add-apt-repository ppa:ondrej/php
+apt-get update -y
+
+# Install apache first
 depInstaller dependencies[@]
 
-if [ $1 == "Y" ]  || [ $1 == "y" ] ; then
-    echo "Installing PHP 7.1"
-    install71
-else
+if [ $VERSION == "latest" ] || [ $VERSION == "" ]; then
     echo "Installing latest php"
     depInstaller phpdeps[@]
+else
+    echo "Installing $VERSION version of php"
+    installOther
 fi
+
 
 #set our webfiles directory to the default var www html directory
 if [ ! -d /vagrant/webfiles ]; then
