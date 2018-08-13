aa6ca6be85aed42e6bb5d4cf3907435054cd5e06
dimezilla <diamond.joshh@gmail.com>
2018-08-13 11:34:52 -0400

    adding some notifications to the setup file so that there is visual confirmation that its working

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 INSTALLPATH=/usr/share/vagrant-create
+
+echo "Installing vagrant-create"
+
 CURDIR=$(dirname $(readlink -f "$0"))
 
 # make our vagrant-create script executable
@@ -12,6 +15,7 @@ else
   rm -rf $INSTALLPATH
 fi
 
+echo "Copying installation files to $INSTALLPATH"
 cp -r $CURDIR $INSTALLPATH
 
 LINKPATH=/usr/bin/vagrant-create
@@ -20,4 +24,11 @@ if [ -L $LINKPATH ]; then
     unlink $LINKPATH
 fi
 
+echo "Setting up command-line utility"
+
 ln -s $INSTALLPATH/vagrant-create.sh $LINKPATH
+
+echo "Thats it! You're good to go."
+
+echo "To make sure it worked run:"
+echo "    > which vagrant-create"
